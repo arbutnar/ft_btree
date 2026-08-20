f53df49e183c289c152557f80bd58cd120a4c9d3
arbutnar <butnaruariannaa@gmail.com>
2026-08-20 15:47:55 +0200

    fixed btree_search_item

--- src/btree_search_item.c
+++ src/btree_search_item.c
@@ -1,12 +1,14 @@
 #include "ft_btree.h"
 
 void    *btree_search_item(t_btree *root, void *data_ref, int (*cmpf)(void *, void *)) {
+    int cmp;
+
     if (root == NULL)
         return NULL;
-    if (btree_search_item(root->left, data_ref, cmpf) == NULL) {
-        if (cmpf(root->item, data_ref) == 0)
-            return root->item;
-        btree_search_item(root->right, data_ref, cmpf);
-    }
-    return NULL;
+    cmp = cmpf(root->item, data_ref);
+    if (cmp == 0)
+        return root->item;
+    if (cmp <= 0)
+        return btree_search_item(root->right, data_ref, cmpf);
+    return btree_search_item(root->left, data_ref, cmpf);
 }
